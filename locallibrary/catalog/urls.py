c184e31771cb53811f9866b50832fa4b96442d52
khanhlyvu30 <khanhlyvu30@gmail.com>
2020-08-07 13:54:58 +0700

    Django Tutorial Part 8: User authentication and permissions

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('authors/', views.AuthorListView.as_view(), name='authors'),
     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+urlpatterns += [
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path('borrowed/',views.AllLoanedBooksListView.as_view(), name='borrowed'),
 ]
-
 
